8994a4c1ea237ca9436d0cec23149e0588bee6f7
harrybaa <harrybaa@gmail.com>
2015-08-29 17:15:29 -0700

    Newly added PASA them
    new them for PASA

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'PASA');
+define('DB_NAME', 'test');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'yhuang45');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
